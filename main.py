523b5b90822bd21585ca7ff200eeeecfd11f0b45
kin0man_ <ilakinsasha2023@gmail.com>
2022-02-06 21:19:26 +0300

    Сделал задачу №4

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import requests
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [650, 550]
+SLOI = {0: 'Схема map', 1: 'Спутник sat', 2: 'Гибрид sat,skl'}
 
 
 class Example(QWidget):
@@ -13,7 +14,6 @@ class Example(QWidget):
         super().__init__()
         self.getImage()
         self.initUI()
-        self.z = 8
 
     def getImage(self):
         self.map_request = "https://static-maps.yandex.ru/1.x/?ll=47.30,56.00&size=650,450&z=8&l=map"
@@ -26,12 +26,21 @@ class Example(QWidget):
             sys.exit(1)
 
     def initUI(self):
+        self.z = 8
+        self.l = 0
         self.setFixedSize(*SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
         self.image = QLabel(self)
+        self.sloi = QPushButton(self)
+        self.sloi.setGeometry(275, 475, 100, 50)
+        self.sloi.setText(SLOI[self.l].split()[0])
+        self.sloi.setFont(QFont("Arial", 15))
+        self.sloi.clicked.connect(self.Sloi_)
         self.out()
 
     def out(self):
+        self.map_request = f"https://static-maps.yandex.ru/1.x/?ll=47.30,56.00&size=650,450&z=" \
+                           f"{self.z}&l={SLOI[self.l % 3].split()[-1]}"
         response = requests.get(self.map_request)
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
@@ -46,12 +55,14 @@ class Example(QWidget):
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageDown and self.z < 23:
             self.z += 1
-            self.map_request = f"https://static-maps.yandex.ru/1.x/?ll=47.30,56.00&size=650,450&z={self.z}&l=map"
-            self.out()
         elif event.key() == Qt.Key_PageUp and self.z > 2:
             self.z -= 1
-            self.map_request = f"https://static-maps.yandex.ru/1.x/?ll=47.30,56.00&size=650,450&z={self.z}&l=map"
-            self.out()
+        self.out()
+
+    def Sloi_(self):
+        self.l += 1
+        self.sloi.setText(SLOI[self.l % 3].split()[0])
+        self.out()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
